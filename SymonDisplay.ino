5c20977bc1bd0cfe149fec3dc3501a4a78e3407f
Denis Dowling <denis.dowling@raedyne.systems>
2020-07-30 21:46:56 +1000

    Initial checkin

---
+++ SymonDisplay.ino
@@ -0,0 +1,231 @@
+enum
+{
+    BUS0 = PA0,
+    BUS1 = PA1,
+    BUS2 = PA2,
+    BUS3 = PA3,
+    BUS4 = PA4,
+    BUS5 = PA5,
+    BUS6 = PA6,
+    BUS7 = PA7,
+
+    COL_SR = PB0,
+    ROW_LATCH = PB1,
+
+    LED = PC13
+};
+
+// Display is organised with one byte per column
+// 64 columns but only 2 - 61 are visible. 0, 1, 62 & 63 not shown
+uint8_t display[64];
+
+void display_clear()
+{
+    memset(display, 0, sizeof(display));
+}
+
+void display_set_x_y(uint8_t x, uint8_t y, bool val)
+{
+    if (x >= sizeof(display))
+        return;
+
+    if (val)
+        display[x] |= (1<<y);
+    else
+        display[x] &= ~(1<<y);
+}
+
+void update_shift_registers(uint8_t row)
+{
+    // Update the column shift registers
+    const uint8_t num_shifts = 8;
+    const uint8_t num_sr = 8;
+    for (uint8_t shift = 0; shift < num_shifts; shift++)
+    {
+        // Set the appropriate bus bits from the display buffer
+        for(uint8_t sr = 0; sr < num_sr; sr++)
+        {
+            uint8_t col = sr * 8 + (num_shifts - shift - 1);
+
+            bool bit = false;
+            if (col < sizeof(display))
+                bit = (display[col] & (1<<row)) != 0;
+
+            uint8_t port;
+            if (sr == 0)
+                port = BUS7;
+            else if (sr == 1)
+                port = BUS6;
+            else if (sr == 2)
+                port = BUS5;
+            else if (sr == 3)
+                port = BUS4;
+            else if (sr == 4)
+                port = BUS3;
+            else if (sr == 5)
+                port = BUS2;
+            else if (sr == 6)
+                port = BUS1;
+            else if (sr == 7)
+                port = BUS0;
+            else
+                continue;
+            digitalWrite(port, bit);
+        }
+        // Move the bus bits into the shift registers
+        digitalWrite(COL_SR, LOW);
+        digitalWrite(COL_SR, HIGH);
+    }
+}
+
+void enable_row(uint8_t row, bool on)
+{
+    // BUS0 nothing
+    // BUS1 nothing
+    // BUS2 nothinh
+    // BUS3 nothing
+    // BUS4 nothing
+    // BUS5 = row 0
+    // BUS6 = row 1
+    // BUS5 | BUS6 = row2
+    // BUS7 = row 3
+    // BUS5 | BUS7 = row4
+    // BUS6 | BUS7 = row5
+    // BUS5 | BUS6 | BUS7 = row6
+
+    bool b5 = false;
+    bool b6 = false;
+    bool b7 = false;
+    if (on)
+    {
+        b5 = ((row + 1) % 2) != 0;
+        b6 = (((row + 1)>>1) % 2) != 0;
+        b7 = (((row + 1)>>2) % 2) != 0;
+    }
+
+    digitalWrite(BUS0, LOW);
+    digitalWrite(BUS1, LOW);
+    digitalWrite(BUS2, LOW);
+    digitalWrite(BUS3, LOW);
+    digitalWrite(BUS4, LOW);
+    digitalWrite(BUS5, b5);
+    digitalWrite(BUS6, b6);
+    digitalWrite(BUS7, b7);
+
+    digitalWrite(ROW_LATCH, LOW);
+    digitalWrite(ROW_LATCH, HIGH);
+}
+
+uint8_t current_row = 0;
+
+// Update event correspond to Rising edge of PWM when configured in PWM1 mode
+void update_callback()
+{
+    enable_row(current_row, false);
+
+    current_row = (current_row + 1) % 8;
+
+    update_shift_registers(current_row);
+    enable_row(current_row, true);
+}
+
+// Compare match event correspond to falling edge of PWM when configured in PWM1 mode
+void compare_callback()
+{
+    enable_row(current_row, false);
+}
+
+HardwareTimer timer(TIM1);
+
+void setup()
+{
+    // Bus pin D0 - D7 map to DIO 2 - 10
+    pinMode(BUS0, OUTPUT);
+    pinMode(BUS1, OUTPUT);
+    pinMode(BUS2, OUTPUT);
+    pinMode(BUS3, OUTPUT);
+    pinMode(BUS4, OUTPUT);
+    pinMode(BUS5, OUTPUT);
+    pinMode(BUS6, OUTPUT);
+    pinMode(BUS7, OUTPUT);
+
+    // Column shift registers clock
+    pinMode(COL_SR, OUTPUT);
+    digitalWrite(COL_SR, HIGH);
+
+    // Row select latch
+    pinMode(ROW_LATCH, OUTPUT);
+    digitalWrite(ROW_LATCH, HIGH);
+
+    // Timer to drive display updates
+    int channel = 1;
+    timer.setMode(channel, TIMER_OUTPUT_COMPARE);
+    timer.setOverflow(800, MICROSEC_FORMAT);
+    //timer.setCaptureCompare(channel, 250, MICROSEC_COMPARE_FORMAT);
+    timer.setCaptureCompare(channel, 10, PERCENT_COMPARE_FORMAT);
+    timer.attachInterrupt(update_callback);
+    timer.attachInterrupt(channel, compare_callback);
+    timer.resume();
+
+    pinMode(LED, OUTPUT);
+
+    display_clear();
+}
+
+extern const unsigned char font5x7[];
+
+const int _width = 64;
+const int _height = 8;
+
+// Draw a character
+void display_char(int16_t x, int16_t y, char c)
+{
+    // Fast clip
+    if((x >= _width) || // Clip right
+       (y >= _height) || // Clip bottom
+       ((x + 6 - 1) < 0) || // Clip left
+       ((y + 8 - 1) < 0))   // Clip top
+        return;
+
+    for (int8_t i = 0; i < 6; i++)
+    {
+        uint8_t v_line;
+        if (i == 5)
+            v_line = 0x0;
+        else
+            v_line = font5x7[c*5+i];
+
+        for (int8_t j = 0; j < 8; j++)
+        {
+            display_set_x_y(x + i, y + j, v_line&1);
+            v_line >>= 1;
+        }
+    }
+}
+
+void display_str(int16_t x, int16_t y, const char *str)
+{
+    while(*str != '\0')
+    {
+        display_char(x, y, *str);
+        str++;
+        x += 6;
+    }
+}
+
+int count = 0;
+
+void loop()
+{
+    display_clear();
+
+    char buf[20];
+    sprintf(buf, "Count=%d", count);
+    display_str(2, 0, buf);
+
+    digitalWrite(LED, count%2);
+
+    count++;
+
+    delay(1000);
+}
---
+++ font5x7.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdint.h>
+
+// Standard ASCII 5x7 font
+extern const uint8_t font5x7[];
